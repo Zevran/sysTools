16fd434e87a765691edd0ed14ed4174ac068e3b9
Zevran <info@zevran.net>
2014-01-15 02:29:16 +0100

    Fix error message

--- install.sh
+++ install.sh
@@ -12,7 +12,9 @@ if [ -d /etc/paths.d ]; then # Mac Os Path predefs
 	echo $CMD >> /etc/paths.d/sysTools
 elif [ -d /etc/profile.d ]; then # Debian Path predefs
 	echo $CMD >> /etc/profile.d/sysTools
-elif [ -f /etc/profile.d/sysTools ] || [ -f /etc/paths.d/sysTools ]; then
+fi
+
+if [ -f /etc/profile.d/sysTools ] || [ -f /etc/paths.d/sysTools ]; then
 	echo "===== Add PATH to PATH env ====="
 else
 	echo "===== Fail to add PATH env ====="
