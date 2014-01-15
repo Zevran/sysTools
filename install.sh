178beb2254ad4e491a44aa604ecab499209b2f4e
Zevran <info@zevran.net>
2014-01-15 02:27:29 +0100

    Keep learning and reading xD

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ echo "===== sysTools installation ====="
 CMD=/opt/sysTools/cmd
 
 if [ -d /etc/paths.d ]; then # Mac Os Path predefs
-	echo CMD >> /etc/paths.d/sysTools
+	echo $CMD >> /etc/paths.d/sysTools
 elif [ -d /etc/profile.d ]; then # Debian Path predefs
-	echo CMD >> /etc/profile.d/sysTools
-elif [ -d /etc/profile.d/sysTools ] || [ -d /etc/paths.d/sysTools ]; then
+	echo $CMD >> /etc/profile.d/sysTools
+elif [ -f /etc/profile.d/sysTools ] || [ -f /etc/paths.d/sysTools ]; then
 	echo "===== Add PATH to PATH env ====="
 else
 	echo "===== Fail to add PATH env ====="
