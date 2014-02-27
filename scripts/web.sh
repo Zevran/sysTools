ac889239b59eb15e914d5d4aac5d1e3796957469
Zevran <info@zevran.net>
2014-02-27 21:33:52 +0100

    Improved user choice for web.sh

--- scripts/web.sh
+++ scripts/web.sh
@@ -10,48 +10,62 @@ apt-get update > /dev/null
 apt-get upgrade -y > /dev/null
 
 echo "[+] Starting web installation script"
-echo "[?] What service would you like as web engine ? :"
-echo "[?] - Nginx [nginx]";echo "[?] - Apache [apache2]"
-read -p "[?] You choice : " string
 
-if [[ "$string" = "nginx" ]]; then
-	echo "[+] Installing nginx..."
-	apt-get install -y nginx > /dev/null
-	engine="nginx"
-elif [[ "$string" = "apache2" ]]; then
-	echo "[+] Installing apache2..."
-	apt-get install -y apache2 > /dev/null
-	engine="apache2"
-else
-	echo "[-] Error: Unknown engine"
-	exit -1
+read -p "[?] Install Web Engine ? : [y/n]" char
+if [[ "$char" = "y" ]]; then
+	echo "[?] What service would you like as web engine ? :"
+	echo "[?] - Nginx [nginx]";echo "[?] - Apache [apache2]"
+	read -p "[?] You choice : " string
+
+	if [[ "$string" = "nginx" ]]; then
+		echo "[+] Installing nginx..."
+		apt-get install -y nginx > /dev/null
+		engine="nginx"
+	elif [[ "$string" = "apache2" ]]; then
+		echo "[+] Installing apache2..."
+		apt-get install -y apache2 > /dev/null
+		engine="apache2"
+	else
+		echo "[-] Error: Unknown engine"
+		exit -1
+	fi
 fi
 
-echo "[+] Installing MySQL..."
-if [[ "$engine" = "nginx" ]]; then
-	echo "[+] Setting up MySQL for nginx"
-	apt-get install -y php5-mysql > /dev/null
-	apt-get install -y mysql-server
-elif [[ "$engine" = "apache2" ]]; then
-	echo "[+] Setting up MySQL for apache2"
-	apt-get install -y libapache2-mod-auth-mysql php5-mysql > /dev/null
-	apt-get install -y mysql-server
+
+read -p "[?] Install MySQL ? : [y/n]" char
+if [[ "$char" = "y" ]]; then
+	echo "[+] Installing MySQL..."
+	if [[ "$engine" = "nginx" ]]; then
+		echo "[+] Setting up MySQL for nginx"
+		apt-get install -y php5-mysql > /dev/null
+		apt-get install -y mysql-server
+	elif [[ "$engine" = "apache2" ]]; then
+		echo "[+] Setting up MySQL for apache2"
+		apt-get install -y libapache2-mod-auth-mysql php5-mysql > /dev/null
+		apt-get install -y mysql-server
+	fi
 fi
 
-echo "[+] Run default script after mysql install"
-sudo mysql_install_db
-echo "[+] Run secure script after mysql install"
-sudo /usr/bin/mysql_secure_installation
+read -p "[?] Run MySQL scripts ? : [y/n]" char
+if [[ "$char" = "y" ]]; then
+	echo "[+] Run default script after mysql install"
+	sudo mysql_install_db
+	echo "[+] Run secure script after mysql install"
+	sudo /usr/bin/mysql_secure_installation
+fi
 
-echo "[+] Installing PHP..."
-apt-get install -y php5
-echo "[+] Select additionnal packages for php"
-apt-cache search php5-
-read -p "[?] Type packages : " packages
-for i in $packages ; do
-	echo "[+] PHP: Installing $i..."
-	apt-get install -y $i > /dev/null
-done
+read -p "[?] Install PHP ? : [y/n]" char
+	if [[ "$char" = "y" ]]; then
+	echo "[+] Installing PHP5..."
+	apt-get install -y php5
+	echo "[+] Select additionnal packages for php"
+	apt-cache search php5-
+	read -p "[?] Type packages : " packages
+	for i in $packages ; do
+		echo "[+] PHP: Installing $i..."
+		apt-get install -y $i > /dev/null
+	done
+fi
 
 read -p "[?] Enable CloudFlare module ? : [y/n]" char
 if [[ "$char" = "y" ]]; then
@@ -79,7 +93,7 @@ fi
 if [[ "$engine" = "nginx" ]]; then
 	echo "[+] Restarting nginx service..."
 	service nginx restart
-else
+elif [[ "$engine" = "apache2" ]]; then
 	echo "[+] Restarting apache2 service..."
 	service apache2 restart
 fi
